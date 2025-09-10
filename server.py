5b4cfd58b9bb91cd9fea9dba8795820c741f2a3b
Sebastien Campion <sebastien.campion@gmail.com>
2025-09-10 16:50:25 +0200

    udpate

--- server.py
+++ server.py
@@ -135,4 +135,4 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
